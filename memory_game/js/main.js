84669ebcf340b6bc7d88c62ad6e16d7f5ccbcf3e
Miriam Espinosa Jorrin <miriam@Miriams-MacBook-Pro.local>
2017-07-28 17:33:36 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,33 @@
 console.log ("Up and running!");
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards [0];
+var cardTwo = cards [2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
+let flipped = function () {
+ for (var i = 0; i < cardsInPlay.length; i++) {
+  //  debugger;
+   if (cardsInPlay[0] === cardsInPlay [1]){
+     return "You found a match!";
+    } else {
+      return "Sorry, try again.";
+    }
+  }
+}
+flipped ();
+
+// Inside of the if statement you created in the last step, create an if...else statement that checks for equality
+//between the two cards in the cardsInPlay array.
+// For the condition, check to see if the first card in the cardsInPlay array (cardsInPlay[0]) is equal to the second card
+// in the cardsInPlay array.
+//
+// Hint: You'll want to use the === operator here.
