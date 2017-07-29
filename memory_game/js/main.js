9f51d9e0e7a4031fd1ce285cbcf6fad43a58d24a
Miriam Espinosa Jorrin <miriam@192-168-1-2.tpgi.com.au>
2017-07-29 12:58:17 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,30 +4,25 @@ console.log ("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards [0];
-var cardTwo = cards [2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-let flipped = function () {
- for (var i = 0; i < cardsInPlay.length; i++) {
-  //  debugger;
-   if (cardsInPlay[0] === cardsInPlay [1]){
-     return "You found a match!";
-    } else {
-      return "Sorry, try again.";
-    }
+var checkForMatch = function (){
+  if (cardsInPlay [0] === cardsInPlay [1]){
+    console.log("You found a match!");
+  }else {
+    console.log("Sorry, try again.");
   }
 }
-flipped ();
 
-// Inside of the if statement you created in the last step, create an if...else statement that checks for equality
-//between the two cards in the cardsInPlay array.
-// For the condition, check to see if the first card in the cardsInPlay array (cardsInPlay[0]) is equal to the second card
-// in the cardsInPlay array.
-//
-// Hint: You'll want to use the === operator here.
+var flipCard = function (cardId){
+  cardsInPlay = cards [cardId];
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+
+  if (cardsInPlay.length === 2){
+  checkForMatch ();
+  }
+
+}
+
+flipCard (0);
+flipCard (1);
