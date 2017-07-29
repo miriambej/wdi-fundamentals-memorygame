53df3720d5d727f24b72fd15f2c98301f94fb6dc
Miriam Espinosa Jorrin <miriam@192-168-1-2.tpgi.com.au>
2017-07-29 18:37:21 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,8 +2,35 @@ console.log ("Up and running!");
 
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+
+];
+
 var cardsInPlay = [];
+
 
 var checkForMatch = function (){
   if (cardsInPlay [0] === cardsInPlay [1]){
@@ -15,14 +42,16 @@ var checkForMatch = function (){
 
 var flipCard = function (cardId){
   cardsInPlay = cards [cardId];
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  //cardsInPlay.push(cards[cardId].rank);
 
-  if (cardsInPlay.length === 2){
-  checkForMatch ();
+    if (cardsInPlay.length === 2){
+      checkForMatch ();
   }
 
-}
+},
 
-flipCard (0);
-flipCard (1);
+flipCard(0);
+flipCard(1);
